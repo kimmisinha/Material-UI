92701a24ed0bf142c731b15febda4109a6b1012c
kimmisinha <kimmi.sinha@newput.com>
2024-07-28 15:47:56 +0530

    Add ButtonGroup vertical and Floating Action Buttons

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Button from "@mui/material/Button";
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import EditIcon from '@mui/icons-material/Edit';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import NavigationIcon from '@mui/icons-material/Navigation';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AcUnitIcon from '@mui/icons-material/AcUnit';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
 function App() {
   return (
     <div className="App">
-      <h1>Text Button</h1>
-      <Button variant="text">Text</Button>
-      <Button color="secondary">Secondary</Button>
-      <Button color="primary" href="https://google.com">Link</Button>
-      <Button disabled>Disabled</Button>
-      <h1>Contained Button</h1>
-      <Button variant='contained'>Click Me</Button>
+    <h1>Text Button</h1>
+    <Button variant="text">Text</Button>
+    <Button color="secondary">Secondary</Button>
+    <Button color="primary" href="https://google.com">Link</Button>
+    <Button disabled>Disabled</Button>
+    <h1>Contained Button</h1>
+    <Button variant='contained'>Click Me</Button>
     <Button variant='contained' color="secondary">Click Me</Button>
     <Button variant='contained' color="primary" href="https://google.com">Link</Button>
     <Button variant='contained' disabled>Disabled</Button>
@@ -33,7 +40,29 @@ function App() {
     <IconButton color="secondary">
       <DeleteIcon/>
     </IconButton>
-    
+    <h1>Custom Button</h1>
+    <Button variant="contained" style={{backgroundColor:"green"}}>Custom1</Button>
+    <Button variant="contained" sx={{backgroundColor:"yellow"}}>Custom2</Button>
+   <h1>Button with click </h1>
+   <Button variant="contained" onClick={() => {alert('clicked'); }}> click me</Button>
+   <h1>Button Group</h1>
+   <ButtonGroup variant="text" aria-label="Basic button group">
+   <Button>One</Button>
+   <Button>Two</Button>
+   <Button>Three</Button>
+   </ButtonGroup>
+   <h1>Button Group vertical</h1>
+   <ButtonGroup variant="text" aria-label="Basic button group" orientation="vertical">
+    <Button>One</Button>
+    <Button>Two</Button>
+    <Button>Three</Button>
+   </ButtonGroup>
+   <h1>Floating action button</h1>
+   <Fab color="warning">R</Fab>
+   <Fab color="Primary"><AddIcon/></Fab>
+   <Fab color="Primary"><EditIcon/></Fab>
+   <Fab disabled><FavoriteIcon/></Fab>
+
     </div>
   );
 }
